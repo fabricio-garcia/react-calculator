dece236563282b26266b67b44cc104c0d37d6314
Fabricio Garcia <gael.fabricio@gmail.com>
2020-05-18 15:15:04 -0500

    feat:  Tie the calculations module with the components and to add the interaction to the buttons
    - Modify App component
    - Modify Button component
    - Modify Button component
    
    Refs. #9

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// eslint-disable-next-line no-unused-vars
-import Calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div id="app">
-      <Display result="0" />
-      <ButtonPanel />
-    </div>
-  );
+// stateful parent
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(name) {
+    const state = calculate(this.state, name);
+    this.setState(state);
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <div id="app">
+        <Display result={total || next} />
+        <ButtonPanel onClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
